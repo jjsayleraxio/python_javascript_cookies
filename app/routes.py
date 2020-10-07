5ce33687d4ed3d8b3f7554720682e1372849b509
Joseph Sayler <joseph.sayler@cytel.com>
2020-10-07 15:57:21 -0700

    add code to set/read session variable/cookie

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,12 @@
-from flask import render_template, flash, redirect, request, url_for, make_response
+from flask import (
+    render_template,
+    flash,
+    redirect,
+    request,
+    url_for,
+    make_response,
+    session,
+)
 from app import app
 from app.forms import LoginForm, cookieSubmitForm
 
@@ -7,21 +15,25 @@ from app.forms import LoginForm, cookieSubmitForm
 @app.route("/index", methods=["GET", "POST"])
 def index():
     user = {"username": "Joseph"}
+    if "USERCOOKIE" not in session:
+        session["USERCOOKIE"] = ""
+    print(session["USERCOOKIE"])
     return render_template("index.html", title="Home", user=user)
 
 
 @app.route("/cookies", methods=["GET", "POST"])
 def set_cookies():
     form = cookieSubmitForm()
+    print(session)
     if request.method == "POST":
         cookie_text = request.form["cookie_text"]
-        print(cookie_text)
-        flash("You entered: {}".format(cookie_text))
-        resp = make_response(
-            render_template("set_cookie.html", title="Cookie Test", form=form)
-        )
-        resp.set_cookie("usercookie", cookie_text)
-        return resp  # redirect(url_for("set_cookies"))
+        session["USERCOOKIE"] = cookie_text
+        # flash("You entered: {}".format(cookie_text))
+        # resp = make_response(
+        #     render_template("set_cookie.html", title="Cookie Test", form=form)
+        # )
+        # resp.set_cookie("usercookie", cookie_text)
+        return redirect(url_for("set_cookies"))
     return render_template("set_cookie.html", title="Cookie Test", form=form)
 
 
