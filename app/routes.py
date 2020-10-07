3b6738b5a0fff3d35d63411677d256e0a6d76091
Joseph Sayler <joseph.sayler@cytel.com>
2020-10-07 13:40:37 -0700

    add new route to set cookie
    added new route to take in the text sent by user and use it to populate
    a cookie that is returned with the response and set in the uesrs browser

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,28 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, request, url_for, make_response
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, cookieSubmitForm
 
 
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods=["GET", "POST"])
+@app.route("/index", methods=["GET", "POST"])
 def index():
     user = {"username": "Joseph"}
     return render_template("index.html", title="Home", user=user)
+
+
+@app.route("/cookies", methods=["GET", "POST"])
+def set_cookies():
+    form = cookieSubmitForm()
+    if request.method == "POST":
+        cookie_text = request.form["cookie_text"]
+        print(cookie_text)
+        flash("You entered: {}".format(cookie_text))
+        resp = make_response(
+            render_template("set_cookie.html", title="Cookie Test", form=form)
+        )
+        resp.set_cookie("usercookie", cookie_text)
+        return resp  # redirect(url_for("set_cookies"))
+    return render_template("set_cookie.html", title="Cookie Test", form=form)
 
 
 @app.route("/login", methods=["GET", "POST"])
